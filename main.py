67409e46b6eeccaec37932878f160faa3c15cd62
Mohamad <alisajadian49@yahoo.com>
2022-06-14 17:13:11 +0430

    develop start semester and it bug

--- main.py
+++ main.py
@@ -2,48 +2,59 @@ from course import Course
 from lecturer import Lecturer
 from student import Student
 
+is_available_add_info = True
+
 while True:
     command = input().split()
 
     if command[0] == 'addStudent':
-        student_id = int(command[1])
-        if Student.check_student_exist(student_id):
-            print('This student has already exist!')
+        if is_available_add_info:
+            student_id = int(command[1])
+            if Student.check_student_exist(student_id):
+                print('This student has already exist!')
+            else:
+                student = Student(student_id)
+                print(f'Student with studentID: {student_id} registered successfully!')
         else:
-            student = Student(student_id)
-            print(f'Student with studentID: {student_id} registered successfully!')
+            print("you can't add this info after start the semester!")
 
     elif command[0] == 'addLecturer':
-        lecturer_id = int(command[1])
-        if Lecturer.check_lecturer_exist(lecturer_id):
-            print("This lecturer has already exist!")
-        else:
-            valid_course = list()
-            if len(command) > 2:
-                # i want to split valid course from all input course
-                list_of_input_courses = command[2:]
-                for course_id in list_of_input_courses:
-                    course = Course.get_course(int(course_id))
-                    if course is not None:
-                        if not Lecturer.check_another_lecturer_present_course(course):
-                            valid_course.append(course)
+        if is_available_add_info:
+            lecturer_id = int(command[1])
+            if Lecturer.check_lecturer_exist(lecturer_id):
+                print("This lecturer has already exist!")
+            else:
+                valid_course = list()
+                if len(command) > 2:
+                    # i want to split valid course from all input course
+                    list_of_input_courses = command[2:]
+                    for course_id in list_of_input_courses:
+                        course = Course.get_course(int(course_id))
+                        if course is not None:
+                            if not Lecturer.check_another_lecturer_present_course(course):
+                                valid_course.append(course)
+                        else:
+                            print("This course doesn't exist")
+                    # it means this lecturer can't register and take the course!
+                    if len(valid_course) == 0:
+                        print('This courses (all of them) taken by another lecturer')
                     else:
-                        print("This course doesn't exist")
-                # it means this lecturer can't register and take the course!
-                if len(valid_course) == 0:
-                    print('This courses (all of them) taken by another lecturer')
-                else:
-                    lecturer = Lecturer(lecturer_id, *valid_course)
-                    print(f"Lecturer with lecturerID: {lecturer_id} registered successfully!")
+                        lecturer = Lecturer(lecturer_id, *valid_course)
+                        print(f"Lecturer with lecturerID: {lecturer_id} registered successfully!")
+        else:
+            print("you can't add this info after start the semester!")
 
     elif command[0] == 'addCourse':
-        course_id = int(command[1])
-        course_unit = int(command[2])
-        if Course.check_course_exist(course_id):
-            print(f"This course with courseID: {course_id} has already exist")
+        if is_available_add_info:
+            course_id = int(command[1])
+            course_unit = int(command[2])
+            if Course.check_course_exist(course_id):
+                print(f"This course with courseID: {course_id} has already exist")
+            else:
+                course = Course(courseID=course_id, unit=course_unit)
+                print(f'This course with courseID: {course_id} registered successfully')
         else:
-            course = Course(courseID=course_id, unit=course_unit)
-            print(f'This course with courseID: {course_id} registered successfully')
+            print("you can't add this info after start the semester!")
 
     elif len(command) > 2 and command[1] == 'register':
         try:
@@ -133,6 +144,8 @@ while True:
         if not lecturer.check_another_lecturer_present_course(course):
             lecturer.set_same_mark_for_all(course, mark)
 
+    elif command[0] == 'start' and command[1] == 'semester':
+        is_available_add_info = False
 
     elif command[0] == 'showAllStudent' and len(command) == 1:
         Student.show_all_student()
